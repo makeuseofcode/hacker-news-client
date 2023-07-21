5f154052923e3ebf42257b1ab3545e60442bacf6
swtbtl <46460041+daviduzondu@users.noreply.github.com>
2023-07-21 13:09:00 +0100

    Update useFetch.jsx

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -7,14 +7,20 @@ export default function useFetch(type, id) {
     useEffect(() => {
         async function fetchData() {
             let response, url, parameter;
-            if (type) { url = "https://node-hnapi.herokuapp.com/"; parameter = type.toLowerCase(); }
-            else if (id) { url = "https://hn.algolia.com/api/v1/items/"; parameter = id.toLowerCase(); }
+            if (type) { 
+                url = "https://node-hnapi.herokuapp.com/"; 
+                parameter = type.toLowerCase(); 
+            }
+            else if (id) { 
+                url = "https://hn.algolia.com/api/v1/items/"; 
+                parameter = id.toLowerCase(); 
+            }
             try {
                 response = await fetch(`${url}${parameter}`);
             } catch (error) {
                 setError(true);
             }
-
+        
             if (response) if (response.status !== 200) {
                 setError(true);
             } else {
