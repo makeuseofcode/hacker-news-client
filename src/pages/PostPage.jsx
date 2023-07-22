443f9c5cf17e5373fd0f7c5801fa6023fe523e97
swtbtl <46460041+daviduzondu@users.noreply.github.com>
2023-07-22 09:20:54 +0100

    Update PostPage.jsx

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -16,20 +16,26 @@ export default function PostPage() {
     }
 
     if (data) {
-        document.title=data.title;
-        return <div>
-            <div className="post-title">{data.title}</div>
-            <div className="post-metadata">
-                {data.url && <Link to={data.url} className="post-link">Visit Website</Link>}
-                <span className="post-author">{data.author}</span>
-                <span className="post-time">{moment(data.created_at).fromNow()}</span>
-            </div>
-            {data.text && <div className="post-text">{parse(data.text)}</div>}
-            <div className="post-comments">
-                <div className="comments-label">Comments</div>
-                <Comments commentsData={data.children} />
-            </div>
+    document.title=data.title;
+    return <div>
+        <div className="post-title">{data.title}</div>
+        <div className="post-metadata">
+            {data.url && 
+            <Link to={data.url} 
+             className="post-link">Visit Website</Link>}
+            <span className="post-author">{data.author}</span>
+            <span className="post-time">
+              {moment(data.created_at).fromNow()}
+            </span>
         </div>
-    }
+        {data.text && 
+        <div className="post-text">
+​​​​​​​        {parse(data.text)}</div>}
+        <div className="post-comments">
+            <div className="comments-label">Comments</div>
+            <Comments commentsData={data.children} />
+        </div>
+    </div>
+}
 }
 
